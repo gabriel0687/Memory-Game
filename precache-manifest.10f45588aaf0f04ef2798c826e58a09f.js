c83ce3fa1124176345e66644905065aa10de022d
jvasquez0687 <jesus_06_06@hotmail.com>
2018-10-17 18:36:39 -0400

    Updates

---
+++ precache-manifest.10f45588aaf0f04ef2798c826e58a09f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aa354a30b0dd1708cc1",
+    "url": "/Memory-Game/static/js/runtime~main.1aa354a3.js"
+  },
+  {
+    "revision": "6f0e0578cc3e4530c3d1",
+    "url": "/Memory-Game/static/js/main.6f0e0578.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/Memory-Game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "6f0e0578cc3e4530c3d1",
+    "url": "/Memory-Game/static/css/main.06d4b779.chunk.css"
+  },
+  {
+    "revision": "b815c347092324d25f2a99f07d874a19",
+    "url": "/Memory-Game/index.html"
+  }
+];
